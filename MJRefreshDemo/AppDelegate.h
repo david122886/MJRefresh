ab62688cd08db87538d6e1b7c1c3b860412b39e6
david.liu <liuchong1228@yahoo.cn>
2013-11-28 16:51:02 +0800

    Initial Commit

---
+++ MJRefreshDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MJRefreshDemo
+//
+//  Created by david on 13-11-28.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MJRefreshDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MJRefreshDemo
+//
+//  Created by david on 13-11-28.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
